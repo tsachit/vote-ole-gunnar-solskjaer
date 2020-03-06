977078e4dde77ba97b0d6522c774bb320b168b84
Sachit Karki <sachitjungkarki@gmail.com>
2020-03-06 21:23:28 +0545

    cleaning url

--- voting-ole.js
+++ voting-ole.js
@@ -2,7 +2,7 @@ const { openBrowser, goto, waitFor, $, click, closeBrowser } = require('taiko');
 (async () => {
     try {
         await openBrowser();
-        await goto("https://www.premierleague.com/news/1634481?fbclid=IwAR0F3U8I2b4h6QRB57tStZ6V4jd40aJ1gti4X3DVOAez2iRn4CcLfxiN0nQ");
+        await goto("https://www.premierleague.com/news/1634481");
 	await waitFor(2000);
         await click($('//main[@id="mainContent"]/div[3]/div[1]/section[1]/div[5]/div/div/div[2]/ul/li[3]/div/span[1]'));
 	await waitFor(2000);
