28c968ad8a1fdf33d797fb20d1fe87032a9aef08
Sachit Karki <sachitjungkarki@gmail.com>
2020-07-03 17:45:21 +0545

    Manager of the month for June 2020

--- voting-ole.js
+++ voting-ole.js
@@ -2,10 +2,11 @@ const { openBrowser, goto, waitFor, $, click, closeBrowser } = require('taiko');
 (async () => {
     try {
         await openBrowser();
-        await goto("https://www.premierleague.com/news/1634481");
-	await waitFor(2000);
-        await click($('//main[@id="mainContent"]/div[3]/div[1]/section[1]/div[5]/div/div/div[2]/ul/li[3]/div/span[1]'));
-	await waitFor(2000);
+        await goto("https://www.premierleague.com/news/1703136");
+        await waitFor("June's Barclays Manager shortlist");
+        await scrollTo("June's Barclays Manager shortlist");
+        await click($('//main[@id="mainContent"]/div[3]/div[1]/section[1]/div[5]/div/div/div[2]/ul/li[4]/div/span[1]'));
+	    await waitFor("Thank you for voting. Results will be announced soon.");
     } catch (error) {
         console.error(error);
     } finally {
